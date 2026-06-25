2f50ecd00f58b4aa7a546999b5bd8c5fa7fa2e85
veloxturtur <92266019+veloxturtur@users.noreply.github.com>
2026-06-25 17:56:48 +0100

    Update hardcoded-scores.js

--- hardcoded-scores.js
+++ hardcoded-scores.js
@@ -23,8 +23,8 @@ const HARDCODED_MATCH_SCORES = {
   // Group C (BRA, MAR, HTI, SCO)
   'g-C-0-0': { homeScore: null, awayScore: null }, // BRA vs MAR
   'g-C-0-1': { homeScore: null, awayScore: null }, // SCO vs HTI
-  'g-C-1-0': { homeScore: 1, awayScore: 0 }, // HTI vs BRA
-  'g-C-1-1': { homeScore: null, awayScore: null }, // MAR vs SCO
+  'g-C-1-0': { homeScore: null, awayScore: null }, // BRA vs SCO
+  'g-C-1-1': { homeScore: 1, awayScore: 0 }, // MAR vs SCO
   'g-C-2-0': { homeScore: null, awayScore: null }, // BRA vs SCO
   'g-C-2-1': { homeScore: null, awayScore: null }, // MAR vs HTI
 
