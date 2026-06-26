5b67f9864fe1c8ffdf413a9436c279800d46fd57
veloxturtur <92266019+veloxturtur@users.noreply.github.com>
2026-06-26 07:08:12 +0100

    Update hardcoded-scores.js

--- hardcoded-scores.js
+++ hardcoded-scores.js
@@ -22,33 +22,33 @@ const HARDCODED_MATCH_SCORES = {
 
   // Group C (BRA, MAR, HTI, SCO)
   'g-C-0-0': { homeScore: 1, awayScore: 1 }, // BRA vs MAR
-  'g-C-0-1': { homeScore: 1, awayScore: 0 }, // SCO vs HTI
-  'g-C-1-0': { homeScore: 3, awayScore: 0 }, // BRA vs SCO
-  'g-C-1-1': { homeScore: 4, awayScore: 2 }, // MAR vs HTI
-  'g-C-2-0': { homeScore: 3, awayScore: 0 }, // BRA vs HTI
-  'g-C-2-1': { homeScore: 1, awayScore: 0 }, // MAR vs SCO
+  'g-C-0-1': { homeScore: 1, awayScore: 0 }, // HTI vs SCO
+  'g-C-1-0': { homeScore: 3, awayScore: 0 }, // BRA vs HTI
+  'g-C-1-1': { homeScore: 1, awayScore: 0 }, // MAR vs SCO
+  'g-C-2-0': { homeScore: 3, awayScore: 0 }, // BRA vs SCO
+  'g-C-2-1': { homeScore: 4, awayScore: 2 }, // MAR vs HTI
 
   // Group D (USA, PAR, AUS, TUR)
   'g-D-0-0': { homeScore: 4, awayScore: 1 }, // USA vs PAR
   'g-D-0-1': { homeScore: 2, awayScore: 0 }, // AUS vs TUR
   'g-D-1-0': { homeScore: 2, awayScore: 0 }, // USA vs AUS
   'g-D-1-1': { homeScore: 1, awayScore: 0 }, // PAR vs TUR
-  'g-D-2-0': { homeScore: null, awayScore: null }, // TUR vs USA
-  'g-D-2-1': { homeScore: null, awayScore: null }, // PAR vs AUS
+  'g-D-2-0': { homeScore: 3, awayScore: 2 }, // TUR vs USA
+  'g-D-2-1': { homeScore: 0, awayScore: 0 }, // PAR vs AUS
 
   // Group E (GER, CUW, CIV, ECU)
   'g-E-0-0': { homeScore: 7, awayScore: 1 }, // GER vs CUW
   'g-E-0-1': { homeScore: 1, awayScore: 0 }, // CIV vs ECU
   'g-E-1-0': { homeScore: 2, awayScore: 1 }, // GER vs CIV
   'g-E-1-1': { homeScore: 0, awayScore: 0 }, // CUW vs ECU
-  'g-E-2-0': { homeScore: null, awayScore: null }, // ECU vs GER
-  'g-E-2-1': { homeScore: null, awayScore: null }, // CUW vs CIV
+  'g-E-2-0': { homeScore: 2, awayScore: 1 }, // ECU vs GER
+  'g-E-2-1': { homeScore: 0, awayScore: 2 }, // CUW vs CIV
 
   // Group F (NED, JPN, TUN, SWE)
   'g-F-0-0': { homeScore: 2, awayScore: 2 }, // NED vs JPN
   'g-F-0-1': { homeScore: 1, awayScore: 5 }, // TUN vs SWE
-  'g-F-1-0': { homeScore: null, awayScore: null }, // NED vs TUN
-  'g-F-1-1': { homeScore: null, awayScore: null }, // JPN vs SWE
+  'g-F-1-0': { homeScore: 3, awayScore: 1 }, // NED vs TUN
+  'g-F-1-1': { homeScore: 1, awayScore: 1 }, // JPN vs SWE
   'g-F-2-0': { homeScore: 5, awayScore: 1 }, // NED vs SWE
   'g-F-2-1': { homeScore: 4, awayScore: 0 }, // JPN vs TUN
 
