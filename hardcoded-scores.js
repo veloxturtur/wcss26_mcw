d3388596aa57cc3e9771ca70bfc551f4978ae25c
veloxturtur <92266019+veloxturtur@users.noreply.github.com>
2026-06-27 09:06:40 +0100

    Update hardcoded-scores.js

--- hardcoded-scores.js
+++ hardcoded-scores.js
@@ -57,16 +57,16 @@ const HARDCODED_MATCH_SCORES = {
   'g-G-0-1': { homeScore: 2, awayScore: 2 }, // IRN vs NZL
   'g-G-1-0': { homeScore: 0, awayScore: 0 }, // BEL vs IRN
   'g-G-1-1': { homeScore: 3, awayScore: 1 }, // EGY vs NZL
-  'g-G-2-0': { homeScore: null, awayScore: null }, // NZL vs BEL
-  'g-G-2-1': { homeScore: null, awayScore: null }, // EGY vs IRN
+  'g-G-2-0': { homeScore: 1, awayScore: 5 }, // NZL vs BEL
+  'g-G-2-1': { homeScore: 1, awayScore: 1 }, // EGY vs IRN
 
   // Group H (ESP, CPV, KSA, URU)
   'g-H-0-0': { homeScore: 0, awayScore: 0 }, // ESP vs CPV
   'g-H-0-1': { homeScore: 1, awayScore: 1 }, // KSA vs URU
   'g-H-1-0': { homeScore: 4, awayScore: 0 }, // ESP vs KSA
   'g-H-1-1': { homeScore: 2, awayScore: 2 }, // CPV vs URU
-  'g-H-2-0': { homeScore: null, awayScore: null }, // URU vs ESP
-  'g-H-2-1': { homeScore: null, awayScore: null }, // CPV vs KSA
+  'g-H-2-0': { homeScore: 0, awayScore: 1 }, // URU vs ESP
+  'g-H-2-1': { homeScore: 0, awayScore: 0 }, // CPV vs KSA
 
   // Group I (FRA, SEN, NOR, IRQ)
   'g-I-0-0': { homeScore: 3, awayScore: 1 }, // FRA vs SEN
