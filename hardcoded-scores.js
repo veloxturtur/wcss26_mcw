fa0f19e0d4ad49d73fb4cb0f4c04c67163794b1f
veloxturtur <92266019+veloxturtur@users.noreply.github.com>
2026-06-28 14:59:29 +0100

    Update hardcoded-scores.js

--- hardcoded-scores.js
+++ hardcoded-scores.js
@@ -81,7 +81,7 @@ const HARDCODED_MATCH_SCORES = {
   'g-J-0-1': { homeScore: 3, awayScore: 1 }, // AUT vs JOR
   'g-J-1-0': { homeScore: 2, awayScore: 0 }, // ARG vs AUT
   'g-J-1-1': { homeScore: 2, awayScore: 1 }, // ALG vs JOR
-  'g-J-2-0': { homeScore: 1, awayScore: 3 }, // JOR vs ARG
+  'g-J-2-0': { homeScore: 3, awayScore: 1 }, // ARG vs JOR
   'g-J-2-1': { homeScore: 3, awayScore: 3 }, // ALG vs AUT
 
   // Group K (POR, COL, UZB, COD)
@@ -96,7 +96,7 @@ const HARDCODED_MATCH_SCORES = {
   'g-L-0-0': { homeScore: 0, awayScore: 0 }, // ENG vs GHA
   'g-L-0-1': { homeScore: 0, awayScore: 1 }, // PAN vs CRO
   'g-L-1-0': { homeScore: 2, awayScore: 0 }, // ENG vs PAN
-  'g-L-1-1': { homeScore: 1, awayScore: 1 }, // GHA vs CRO
+  'g-L-1-1': { homeScore: 1, awayScore: 2 }, // GHA vs CRO
   'g-L-2-0': { homeScore: 4, awayScore: 2 }, // ENG vs CRO
   'g-L-2-1': { homeScore: 1, awayScore: 0 }, // GHA vs PAN
 
