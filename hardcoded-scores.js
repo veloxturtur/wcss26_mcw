bcf14242685bcdf4ee6647d50bfc6a93f9acb847
veloxturtur <92266019+veloxturtur@users.noreply.github.com>
2026-07-05 00:06:51 +0100

    Update hardcoded-scores.js

--- hardcoded-scores.js
+++ hardcoded-scores.js
@@ -124,8 +124,8 @@ const HARDCODED_MATCH_SCORES = {
   'ko-r32-16': { stage: 'r32', home: 'ARG', away: 'CPV', homeScore: 3, awayScore: 2 },
 
   // Round of 16 (July 4 – July 7, 2026)
-  'ko-r16-1': { stage: 'r16', date: '2026-07-04T19:00', home: 'CAN', away: 'MAR', homeScore: 0, awayScore: 3 },
-  'ko-r16-2': { stage: 'r16', date: '2026-07-04T22:00', home: 'PAR', away: 'FRA', homeScore: null, awayScore: null },
+  'ko-r16-1': { stage: 'r16', date: '2026-07-04T19:00', home: 'MAR', away: 'CAN', homeScore: 3, awayScore: 0 },
+  'ko-r16-2': { stage: 'r16', date: '2026-07-04T22:00', home: 'PAR', away: 'FRA', homeScore: 0, awayScore: 1 },
   'ko-r16-3': { stage: 'r16', date: '2026-07-05T19:00', home: 'BEL', away: 'USA', homeScore: null, awayScore: null },
   'ko-r16-4': { stage: 'r16', date: '2026-07-05T22:00', home: 'ESP', away: 'POR', homeScore: null, awayScore: null },
   'ko-r16-5': { stage: 'r16', date: '2026-07-06T19:00', home: 'BRA', away: 'NOR', homeScore: null, awayScore: null },
@@ -135,7 +135,7 @@ const HARDCODED_MATCH_SCORES = {
 
   // Quarterfinals (July 9 – July 10, 2026)
   'ko-qf-1': { stage: 'qf', date: '2026-07-09T19:00', home: 'MAR', away: 'TBD', homeScore: null, awayScore: null },
-  'ko-qf-2': { stage: 'qf', date: '2026-07-09T22:00', home: 'TBD', away: 'TBD', homeScore: null, awayScore: null },
+  'ko-qf-2': { stage: 'qf', date: '2026-07-09T22:00', home: 'FRA', away: 'TBD', homeScore: null, awayScore: null },
   'ko-qf-3': { stage: 'qf', date: '2026-07-10T19:00', home: 'TBD', away: 'TBD', homeScore: null, awayScore: null },
   'ko-qf-4': { stage: 'qf', date: '2026-07-10T22:00', home: 'TBD', away: 'TBD', homeScore: null, awayScore: null },
 
